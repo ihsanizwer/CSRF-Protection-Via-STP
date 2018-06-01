95ec45b362ec9c54cbe922dabf4bba03246f2993
Mohamed Ihsan Izwer <ihsanizwer@yahoo.com>
2018-06-01 17:07:38 +0530

    RNG added

--- src/main/java/servlet/SessionCreator.java
+++ src/main/java/servlet/SessionCreator.java
@@ -4,12 +4,26 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.security.SecureRandom;
+
 import data.SessionToCSRFMap;
 
 public class SessionCreator extends HttpServlet {
+    byte bytearray [] = new byte[20];
 
-        public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            //Write stuff here
-        }
+    SecureRandom random = new SecureRandom();
+
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        HttpSession session = request.getSession();
+        session.setAttribute("SomeBankSes_ID", bytearray );
+        System.out.println(bytearray);
+        random.nextBytes(bytearray);
+        int a =5;
+        //Write stuff here
+    }
 }
