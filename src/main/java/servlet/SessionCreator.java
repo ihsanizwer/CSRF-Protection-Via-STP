b20b105ce963219345592b7dc9e1881e9a579dce
Mohamed Ihsan Izwer <ihsanizwer@yahoo.com>
2018-06-06 10:56:13 +0530

    Changed pom.xml with required configs such as name of the file and type. Corrected issues in the login servlet

--- src/main/java/servlet/SessionCreator.java
+++ src/main/java/servlet/SessionCreator.java
@@ -18,10 +18,20 @@ public class SessionCreator extends HttpServlet {
 
         String sess ="";
         String csrfToken ="";
+        boolean isLogged = false;
 
         HttpSession session = request.getSession(false);
 
-        if(session == null) {
+        Cookie [] cookies = request.getCookies();
+        for(Cookie cookie: cookies){
+            if(cookie.getName().equals("STPSesID")){
+                if(stcm.isLoggedIn(cookie.getValue())){
+                    isLogged = true;
+                }
+            }
+        }
+
+        if(!isLogged) {
             //Will be authenticating user and Making a new session since We don't have a session at this moment
             String username = request.getParameter("user");
             String password = request.getParameter("pass");
@@ -58,19 +68,9 @@ public class SessionCreator extends HttpServlet {
             }
 
         }else{
-            //Session exist verifying session
-            boolean authentic = false;
-            Cookie [] cookies = request.getCookies();
-            for(Cookie cookie: cookies){
-                if(cookie.getName().equals("STPSesID")){
-                    if(stcm.isLoggedIn(cookie.getValue())){
-                        authentic = true;
-                    }
-                }
-            }
-            if(authentic)
+
                 response.sendRedirect("home.jsp");
-            else{
+                /*
                 response.setContentType("text/html");
                 response.setCharacterEncoding("UTF-8");
 
@@ -81,7 +81,7 @@ public class SessionCreator extends HttpServlet {
                         .append("<h1>Error: Please delete your browser cookies and <a href='index.jsp'> try again!</a>\r\n")
                         .append("</body>\r\n")
                         .append("</html>\r\n");
-            }
+                */
 
         }
 
